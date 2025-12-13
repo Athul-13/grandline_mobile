a8b787ac122003a609556d3e1c08234012f084ef
Athul-13 <kkunnikrishnan13@gmail.com>
2025-12-13 18:41:49 +0530

    docs: add comprehensive README files for all modules

--- src/hooks/auth/index.ts
+++ src/hooks/auth/index.ts
@@ -1,9 +1,5 @@
-export { useAuth } from './use_auth';
-export { useChangePassword } from './use_change_password';
-export { useForgotPassword } from './use_forgot_password';
 export { useLogin } from './use_login';
 export { useLogout } from './use_logout';
-export { useResetPassword } from './use_reset_password';
+export { useAuth } from './use_auth';
+export { useChangePassword } from './use_change_password';
 
-
-export { useUpdateOnboardingPassword } from './use_update_onboarding_password';
